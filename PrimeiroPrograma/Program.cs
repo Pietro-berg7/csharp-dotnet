a38cb18f924f0e5e3da5333a84f167539f4e5406
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-05 23:20:38 +0044

    Hello World

---
+++ PrimeiroPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
