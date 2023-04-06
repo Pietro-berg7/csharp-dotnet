7b9777e8e6dd1cd9a7b2545d328926a8216cac3a
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-06 16:40:44 +0044

    Conversão implícita e casting

---
+++ 2_LOGICA_C#/4_conversao_implicita_e_casting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            float x = 4.5f;
+            double y = x;
+            Console.WriteLine(y);
+
+            //double a;
+            //float b;
+
+            //a = 5.1;
+            //b = (float)a;
+            //Console.WriteLine(b);
+
+            //double a;
+            //int b;
+
+            //a = 5.1;
+            //b = (int)a;
+            //Console.WriteLine(b);
+
+            int a = 5;
+            int b = 2;
+
+            double result = (double) a / b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
