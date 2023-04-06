f9367e2b397503174396335b1d3d8cc496fbe701
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-06 17:18:54 +0044

    Entrada de dados parte 1

--- 2_LOGICA_C#/4_operadores_aritmeticos/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Course {
-    class Program {
-        static void Main(string[] args) {
-
-        }
-    }
-}
---
+++ 2_LOGICA_C#/6_entrada_de_dados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
