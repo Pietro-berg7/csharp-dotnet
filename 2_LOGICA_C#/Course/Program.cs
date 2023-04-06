f5996bac90cf675ac08326456222b576f37698d9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-06 16:01:21 +0044

    Saída de dados

--- 2_LOGICA_C#/Course/Program.cs
+++ 2_LOGICA_C#/Course/Program.cs
@@ -1,61 +1,58 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course {
     internal class Program {
         static void Main(string[] args) {
 
-            int n1 = int.MinValue;
-            int n2 = int.MaxValue;
-            sbyte n3 = sbyte.MinValue;
-            decimal n4 = decimal.MaxValue;
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
 
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
 
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
 
+            Console.WriteLine($"Produtos: \n" +
+                $"{produto1}, cujo preço é $ {preco1:F2}\n" +
+                $"{produto2}, cujo preço é $ {preco2:F2}\n");
 
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}\n");
 
-            //SByte x = 100;
-            //byte n1 = 256;
-            //byte n1 = -1;
-            //byte n1 = 255;
-            //n1++; result => 0
-
-
-
-
-            //byte n1 = 126;
-            //int n2 = 1000;
-            //int n3 = 2147483647;
-            //long n4 = 2147483648L;
-
-            //Console.WriteLine(n1);
-            //Console.WriteLine(n2);
-            //Console.WriteLine(n3);
-            //Console.WriteLine(n4);
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}\n" +
+                $"Arredondado (três casas decimais): {medida:F3}\n" +
+                "Separador decimal invariant culture: {0}", medida.ToString("F3", CultureInfo.InvariantCulture));
 
 
 
-
-            //bool completo = false;
             //char genero = 'M';
-            //char letra = '\u0041';
-            //float n5 = 4.5f;
-            //double n6 = 4.5;
-            //string nome = "Pietro";
-            //object obj1 = "Betotle";
-            //object obj2 = 4.5f;
+            //int idade = 32;
+            //double saldo = 10.35784;
+            //string nome = "Betotle";
 
-            //Console.WriteLine(completo);
+            //Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            //Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+
+
+
+
+            //Console.Write("Bom dia!");
+            //Console.WriteLine("Boa tarde!");
+            //Console.WriteLine("Boa noite!");
+            //Console.WriteLine("-----------------------------");
             //Console.WriteLine(genero);
-            //Console.WriteLine(letra);
-            //Console.WriteLine(n5);
-            //Console.WriteLine(n6);
+            //Console.WriteLine(idade);
+            //Console.WriteLine(saldo);
             //Console.WriteLine(nome);
-            //Console.WriteLine(obj1);
-            //Console.WriteLine(obj2);
+            //Console.WriteLine(saldo.ToString("F2"));
+            //Console.WriteLine(saldo.ToString("F4"));
+            //Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
 }
