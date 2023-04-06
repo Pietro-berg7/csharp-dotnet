872590ba2b9bc8e66bd01ae8a529475439c53479
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-06 17:02:32 +0044

    Operadores aritméticos

---
+++ 2_LOGICA_C#/5_operadores_aritmeticos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+            //double n4 = 10.0 / 8.0;
+            double n4 = (double)10 / 8;
+
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
+        }
+    }
+}
