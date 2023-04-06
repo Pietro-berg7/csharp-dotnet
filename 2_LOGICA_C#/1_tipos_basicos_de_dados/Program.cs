9bbd1e2d455c7db4db2d4e034f379247710e9c4e
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-06 16:31:33 +0044

    Operadores de atribuição

---
+++ 2_LOGICA_C#/1_tipos_basicos_de_dados/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Course {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            decimal n4 = decimal.MaxValue;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+
+
+
+            //sbyte x = 100;
+            //byte n1 = 256;
+            //byte n1 = -1;
+            //byte n1 = 255;
+            //n1++; result => 0
+
+
+
+
+            //byte n1 = 126;
+            //int n2 = 1000;
+            //int n3 = 2147483647;
+            //long n4 = 2147483648l;
+
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+
+
+
+
+            bool completo = false;
+            char genero = 'm';
+            char letra = '\u0041';
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "pietro";
+            object obj1 = "betotle";
+            object obj2 = 4.5f;
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+        }
+    }
+}
---
+++ 2_LOGICA_C#/3_operadores_de_atribuicao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 10;
+            //int b = a++;
+            int b = ++a;
+
+            Console.WriteLine(a);
+            //Console.WriteLine(b); // 10
+            Console.WriteLine(b); // 11
+
+
+            //int a = 10;
+            //Console.WriteLine(a);
+
+            //a += 2;
+            //Console.WriteLine(a);
+
+            //a *= 3;
+            //Console.WriteLine(a);
+
+            //string s = "ABC";
+            //Console.WriteLine(s);
+
+            //s += "DEF";
+            //Console.WriteLine(s);
+
+        }
+    }
+}
