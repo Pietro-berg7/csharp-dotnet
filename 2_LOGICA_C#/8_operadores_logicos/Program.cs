e8a4f1493bc1f9c7fab82d1f4933c9fd4c31113c
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-12 19:04:58 +0044

    Operadores lógicos

---
+++ 2_LOGICA_C#/8_operadores_logicos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            bool c1 = 2 > 3 && 4 != 5;
+            bool c2 = 2 > 3 || 4 != 5; // true
+            bool c3 = !(2 > 3) && 4 != 5; // true
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine("------------------------------");
+
+            bool c4 = 10 < 5; // false
+            bool c5 = c2 || c3 && c4;
+
+            Console.WriteLine(c4);
+            Console.WriteLine(c5);
+
+        }
+    }
+}
