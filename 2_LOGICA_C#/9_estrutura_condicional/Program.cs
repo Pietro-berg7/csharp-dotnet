0e01f582920b80e0e4bc8ae89985f4b8684278e6
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-12 19:28:23 +0044

    Estrutura condicional

---
+++ 2_LOGICA_C#/9_estrutura_condicional/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            // ---- Estrutura condicional - Encadeamentos ----
+
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora >= 12 && hora < 18) {
+                Console.WriteLine("Boa tarde!");
+            }
+            else {
+                Console.WriteLine("Boa noite!");
+            }
+
+
+
+
+            // ---- Estrutura condicional composta ----
+
+            //Console.WriteLine("Entre com um número inteiro:");
+            //int x = int.Parse(Console.ReadLine());
+
+            //if (x % 2 == 0) {
+            //    Console.WriteLine("Par!");
+            //} else {
+            //    Console.WriteLine("Ímpar!");
+            //}
+
+
+
+
+            // ---- Estrutura condicional simples ----
+
+            //int x = 10;
+
+            //Console.WriteLine("Bom dia!");
+
+            //if (x > 5) {
+            //    Console.WriteLine("Boa tarde!");
+            //}
+
+            //Console.WriteLine("Boa noite!");
+
+        }
+    }
+}
