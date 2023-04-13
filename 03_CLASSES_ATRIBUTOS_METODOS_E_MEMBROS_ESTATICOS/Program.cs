b671c06a7b2e34f0eadb2f4ed0f7d48db40f623d
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-13 04:27:40 +0044

    3-2: Criando uma classe com três atributos para representar melhor um Triângulo

---
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace _03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS {
+    class Program {
+        static void Main(string[] args) {
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture)); 
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior área: X");
+            } else {
+                Console.WriteLine("Maior área: Y");
+            }
+
+        }
+    }
+}
---
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS {
+    internal class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;    
+
+    }
+}
