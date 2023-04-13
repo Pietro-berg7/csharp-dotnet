31f8c2e8b3916e4bbad27e75067842de988f5525
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-13 21:06:33 +0044

    3-6: Object e ToString

--- 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/SEGUNDO_PROBLEMA/Produto.cs
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/SEGUNDO_PROBLEMA/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,16 @@ namespace SEGUNDO_PROBLEMA {
 
         public double ValorTotalEmEstoque() {
             return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
 
     }
