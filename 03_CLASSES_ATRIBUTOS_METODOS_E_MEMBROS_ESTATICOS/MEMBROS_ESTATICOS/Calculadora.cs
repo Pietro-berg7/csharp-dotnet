61c3d5c089a5bfb5e1ed0c514e7984e6df1e5ace
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-15 00:16:54 +0044

    3-10: Membros estáticos - Parte 2

---
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEMBROS_ESTATICOS {
+    internal class Calculadora {
+
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double raio) {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+
+
+    }
+}
--- 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/Program.cs
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/Program.cs
@@ -2,30 +2,18 @@
 
 namespace MEMBROS_ESTATICOS {
     class Program {
-
-        static double Pi = 3.14;
-
         static void Main(string[] args) {
 
             Console.Write("Entre o valor do raio: ");
             double raio = Convert.ToDouble(Console.ReadLine());
 
-            double circ = Circunferencia(raio);
-            double vol = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2"));
             Console.WriteLine("Volume: " + vol.ToString("F2"));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2"));
 
         }
-
-        static double Circunferencia(double raio) {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume(double r) {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
-        }
-
     }
 }
