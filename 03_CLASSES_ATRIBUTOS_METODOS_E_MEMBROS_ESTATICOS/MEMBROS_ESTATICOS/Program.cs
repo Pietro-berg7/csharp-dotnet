914d89f1871e0cf629b5d1b7f35044747c496c36
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-15 00:09:37 +0044

    3-9: Membros estáticos - Parte 1

---
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MEMBROS_ESTATICOS {
+    class Program {
+
+        static double Pi = 3.14;
+
+        static void Main(string[] args) {
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            double circ = Circunferencia(raio);
+            double vol = Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + vol.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2"));
+
+        }
+
+        static double Circunferencia(double raio) {
+            return 2.0 * Pi * raio;
+        }
+
+        static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+
+    }
+}
--- 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/SEGUNDO_PROBLEMA/Program.cs
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/SEGUNDO_PROBLEMA/Program.cs
@@ -5,34 +5,34 @@ namespace SEGUNDO_PROBLEMA {
     class Program {
         static void Main(string[] args) {
 
-            //Produto p = new Produto();
+            Produto p = new Produto();
 
-            //Console.WriteLine("Entre os dados do produto:");
-            //Console.Write("Nome: ");
-            //p.Nome = Console.ReadLine();
-            //Console.Write("Preço: ");
-            //p.Preco = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //Console.Write("Quantidade no estoque: ");
-            //p.Quantidade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine();
-            //Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
 
-            //Console.WriteLine();
-            //Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            //int qte = Convert.ToInt32(Console.ReadLine());
-            //p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = Convert.ToInt32(Console.ReadLine());
+            p.AdicionarProdutos(qte);
 
-            //Console.WriteLine();
-            //Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
 
-            //Console.WriteLine();
-            //Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            //qte = Convert.ToInt32(Console.ReadLine());
-            //p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = Convert.ToInt32(Console.ReadLine());
+            p.RemoverProdutos(qte);
 
-            //Console.WriteLine();
-            //Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
 
 
 
@@ -72,16 +72,16 @@ namespace SEGUNDO_PROBLEMA {
 
 
             // ---- Exercício 3 ----
-            Aluno a = new Aluno();
+            //Aluno a = new Aluno();
 
-            Console.Write("Nome do aluno: ");
-            a.Nome = Console.ReadLine();
-            Console.WriteLine("Digite as três notas do aluno:");
-            a.Nota1 = Convert.ToDouble(Console.ReadLine());
-            a.Nota2 = Convert.ToDouble(Console.ReadLine());
-            a.Nota3 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Nome do aluno: ");
+            //a.Nome = Console.ReadLine();
+            //Console.WriteLine("Digite as três notas do aluno:");
+            //a.Nota1 = Convert.ToDouble(Console.ReadLine());
+            //a.Nota2 = Convert.ToDouble(Console.ReadLine());
+            //a.Nota3 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(a);
+            //Console.WriteLine(a);
 
         }
     }
