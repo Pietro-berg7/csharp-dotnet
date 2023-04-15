cc3a010b8598ecde228ef740f5bb01daace00cc2
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-15 00:53:02 +0044

    3-11: Exercício

---
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEMBROS_ESTATICOS {
+    internal class ConversorDeMoeda {
+
+        public static double IOF = 6.0;
+
+        public static double DolarParaReal(double cotacao, double quantidade) {
+            double total = cotacao * quantidade;
+            return total += total * IOF / 100;
+        }
+
+    }
+}
--- 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/Program.cs
+++ 03_CLASSES_ATRIBUTOS_METODOS_E_MEMBROS_ESTATICOS/MEMBROS_ESTATICOS/Program.cs
@@ -4,15 +4,27 @@ namespace MEMBROS_ESTATICOS {
     class Program {
         static void Main(string[] args) {
 
-            Console.Write("Entre o valor do raio: ");
-            double raio = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Entre o valor do raio: ");
+            //double raio = Convert.ToDouble(Console.ReadLine());
 
-            double circ = Calculadora.Circunferencia(raio);
-            double vol = Calculadora.Volume(raio);
+            //double circ = Calculadora.Circunferencia(raio);
+            //double vol = Calculadora.Volume(raio);
 
-            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
-            Console.WriteLine("Volume: " + vol.ToString("F2"));
-            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2"));
+            //Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            //Console.WriteLine("Volume: " + vol.ToString("F2"));
+            //Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2"));
+
+
+
+
+            // ---- Exercício ----
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = Convert.ToDouble(Console.ReadLine());
+            double valor = ConversorDeMoeda.DolarParaReal(cotacao, quantidade);
+
+            Console.Write("Valor a ser pago em reais = " + valor.ToString("F2"));
 
         }
     }
