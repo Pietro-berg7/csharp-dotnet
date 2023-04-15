a0087a3d78e7a1707373ee084690b0cd3083391d
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-15 21:36:19 +0044

    4-5: Encapsulamento

--- 04_CONSTRUTORES_THIS_SOBRECARGA_E_ENCAPSULAMENTO/Course/Course/Produto.cs
+++ 04_CONSTRUTORES_THIS_SOBRECARGA_E_ENCAPSULAMENTO/Course/Course/Produto.cs
@@ -2,41 +2,55 @@
 namespace Course {
     class Produto {
 
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto() {
-            Quantidade = 10;
         }
 
-        public Produto(string nome, double preco) : this() {
-            Nome = nome;
-            Preco = preco;
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
 
-        public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
-            Quantidade= quantidade;
+        public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
         }
 
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString() {
-            return Nome
+            return _nome
                 + ", $ "
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- 04_CONSTRUTORES_THIS_SOBRECARGA_E_ENCAPSULAMENTO/Course/Course/Program.cs
+++ 04_CONSTRUTORES_THIS_SOBRECARGA_E_ENCAPSULAMENTO/Course/Course/Program.cs
@@ -4,41 +4,13 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Entre os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Produto p = new Produto("TV", 500.00, 10);
 
-            Produto p = new Produto(nome, preco);
+            p.SetNome("T");
 
-            Produto p2 = new Produto();
-
-            Produto p3 = new Produto {
-                Nome = "TV",
-                Preco = 500.00,
-                Quantidade = 20
-            };
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
 
         }
     }
