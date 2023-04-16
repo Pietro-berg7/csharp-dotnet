5458e5093457496b9abc87caaa8fadf301e679ed
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-16 17:33:50 +0044

    5-1: Tipos referência e tipos valor

---
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/tipos_referencia_tipos_valor/tipos_referencia_tipos_valor/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipos_referencia_tipos_valor {
+    struct Point {
+
+        public double X;
+        public double Y;
+
+        public override string ToString() {
+            return "(" + X + ", " + Y + ")";
+        }
+
+    }
+}
---
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/tipos_referencia_tipos_valor/tipos_referencia_tipos_valor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tipos_referencia_tipos_valor {
+    class Program {
+        static void Main(string[] args) {
+
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+
+            p = new Point();
+            Console.WriteLine(p);
+
+        }
+    }
+}
