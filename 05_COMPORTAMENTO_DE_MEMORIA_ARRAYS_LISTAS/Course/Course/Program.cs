dec726a0b634fa835241c9ede350c0e84bbe13d3
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-16 21:39:43 +0044

    5-2: Nullable

---
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Course/Course/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            //Nullable<double> x = null;
+            //double? x = null;
+            //double? y = 10.0;
+
+            //Console.WriteLine(x.GetValueOrDefault());
+            //Console.WriteLine(y.GetValueOrDefault());
+
+            //Console.WriteLine(x.HasValue);
+            //Console.WriteLine(y.HasValue);
+
+            //if (x.HasValue) {
+            //    Console.WriteLine(x.Value);
+            //}
+            //else {
+            //    Console.WriteLine("X id null");
+            //}
+
+            //if (y.HasValue) {
+            //    Console.WriteLine(y.Value);
+            //}
+            //else {
+            //    Console.WriteLine("Y id null");
+            //}
+
+            double? x = null;
+            double? y = 10;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
