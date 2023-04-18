e6c8a20fbc6589b28532e9f83ca02d26c735bacd
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-18 17:27:53 -0300

    5-13: Matrizes - Exercício

--- 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Matrizes/Matrizes/Program.cs
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Matrizes/Matrizes/Program.cs
@@ -4,34 +4,79 @@ namespace Matrizes {
     class Program {
         static void Main(string[] args) {
 
-            int n = Convert.ToInt32(Console.ReadLine());
+            string[] values = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(values[0]);
+            int m = Convert.ToInt32(values[1]);
 
-            int[,] mat = new int[n, n];
+            int[,] mat = new int[n, m];
 
             for (int i = 0; i < n; i++) {
+                string[] line = Console.ReadLine().Split(' ');
 
-                string[] values = Console.ReadLine().Split(' ');
-
-                for (int j = 0; j < n; j++) {
-                    mat[i, j] = Convert.ToInt32(values[j]);
+                for (int j = 0; j < m; j++) {
+                    mat[i, j] = Convert.ToInt32(line[j]);
                 }
             }
 
-            Console.WriteLine("Main diagonal:");
-            for (int i = 0; i < n; i++) {
-                Console.Write(mat[i, i] + " ");
-            }
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            int count = 0;
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (mat[i, j] < 0) {
-                        count++;
+                for (int j = 0; j < m; j++) {
+                    if (mat[i, j] == number) {
+                        Console.WriteLine("Position {0},{1}:", i, j);
+
+                        if (j - 1 >= 0) {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (j + 1 < m) {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i - 1 >= 0) {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (i + 1 < n) {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
                     }
                 }
             }
 
-            Console.WriteLine("\nNegative numbers: " + count);
+
+
+
+
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int[,] mat = new int[n, n];
+
+            //for (int i = 0; i < n; i++) {
+
+            //    string[] values = Console.ReadLine().Split(' ');
+
+            //    for (int j = 0; j < n; j++) {
+            //        mat[i, j] = Convert.ToInt32(values[j]);
+            //    }
+            //}
+
+            //Console.WriteLine("Main diagonal:");
+            //for (int i = 0; i < n; i++) {
+            //    Console.Write(mat[i, i] + " ");
+            //}
+
+            //int count = 0;
+            //for (int i = 0; i < n; i++) {
+            //    for (int j = 0; j < n; j++) {
+            //        if (mat[i, j] < 0) {
+            //            count++;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("\nNegative numbers: " + count);
+
+
+
 
 
 
