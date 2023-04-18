7189fa23a194691da9df117e75c6177302795f1f
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-18 15:33:31 -0300

    5-10: Listas - Exercício

---
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Lists/Lists/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists {
+    internal class Employee {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increaseSalary(double percentage) {
+            Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString() {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
--- 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Lists/Lists/Program.cs
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Lists/Lists/Program.cs
@@ -1,54 +1,100 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Lists {
     class Program {
         static void Main(string[] args) {
 
-            List<string> list = new List<string>();
+            List<Employee> list = new List<Employee>();
 
-            list.Add("Maria");
-            list.Add("Alex");
-            list.Add("Bob");
-            list.Add("Ana");
-            list.Insert(2, "Marco");
+            Console.Write("How many employees will be registered? ");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Employee #{0}:", i + 1);
+
+                Console.Write("Id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Employee (id, name, salary));
+                Console.WriteLine();
             }
 
-            Console.WriteLine("List count: " + list.Count);
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int findId = Convert.ToInt32(Console.ReadLine());
 
-            string s1 = list.Find(x => x[0] == 'A');
-            Console.WriteLine("First 'A': " + s1);
+            Employee exists = list.Find(x => x.Id == findId);
 
-            string s2 = list.FindLast(x => x[0] == 'A');
-            Console.WriteLine("Last 'A': " + s2);
+            if (exists != null) {
+                Console.Write("Enter the percentage: ");
+                double percentage = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            int pos1 = list.FindIndex(x => x[0] == 'A');
-            Console.WriteLine("First position 'A': " + pos1);
-
-            int pos2 = list.FindLastIndex(x => x[0] == 'A');
-            Console.WriteLine("Last position 'A': " + pos2);
-
-            List<string> list2 = list.FindAll(x => x.Length == 5);
-            Console.WriteLine("-------------------------------");
-            foreach (string obj in list2) {
-                Console.WriteLine(obj);
+                exists.increaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id does not exists!");
             }
 
-            list.Remove("Alex");
-            //list.RemoveAt(3);
-            //list.RemoveRange(2, 2);
-            Console.WriteLine("-------------------------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
+            Console.WriteLine("\nUpdated list of employees:");
+            foreach(Employee emp in list) {
+                Console.WriteLine(emp);
             }
 
-            list.RemoveAll(x => x[0] == 'M');
-            Console.WriteLine("-------------------------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
-            }
+
+
+
+
+            //List<string> list = new List<string>();
+
+            //list.Add("Maria");
+            //list.Add("Alex");
+            //list.Add("Bob");
+            //list.Add("Ana");
+            //list.Insert(2, "Marco");
+
+            //foreach (string obj in list) {
+            //    Console.WriteLine(obj);
+            //}
+
+            //Console.WriteLine("List count: " + list.Count);
+
+            //string s1 = list.Find(x => x[0] == 'A');
+            //Console.WriteLine("First 'A': " + s1);
+
+            //string s2 = list.FindLast(x => x[0] == 'A');
+            //Console.WriteLine("Last 'A': " + s2);
+
+            //int pos1 = list.FindIndex(x => x[0] == 'A');
+            //Console.WriteLine("First position 'A': " + pos1);
+
+            //int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            //Console.WriteLine("Last position 'A': " + pos2);
+
+            //List<string> list2 = list.FindAll(x => x.Length == 5);
+            //Console.WriteLine("-------------------------------");
+            //foreach (string obj in list2) {
+            //    Console.WriteLine(obj);
+            //}
+
+            //list.Remove("Alex");
+            ////list.RemoveAt(3);
+            ////list.RemoveRange(2, 2);
+            //Console.WriteLine("-------------------------------");
+            //foreach (string obj in list) {
+            //    Console.WriteLine(obj);
+            //}
+
+            //list.RemoveAll(x => x[0] == 'M');
+            //Console.WriteLine("-------------------------------");
+            //foreach (string obj in list) {
+            //    Console.WriteLine(obj);
+            //}
 
         }
     }
