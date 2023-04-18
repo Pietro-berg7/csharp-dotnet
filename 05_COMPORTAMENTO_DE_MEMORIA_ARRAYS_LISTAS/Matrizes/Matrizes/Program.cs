c34ba9be775dae2d96c84c29b7cae49e4922d38a
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-18 15:41:51 -0300

    5-11: Matrizes

---
+++ 05_COMPORTAMENTO_DE_MEMORIA_ARRAYS_LISTAS/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            Double[,] mat = new Double[2, 3];
+
+            Console.WriteLine(mat.Length);
+            
+            Console.WriteLine(mat.Rank);
+
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
+
+
+        }
+    }
+}
