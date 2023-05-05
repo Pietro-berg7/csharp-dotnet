c1122db3de2d2df6b5dba0dd38949174bcfeabc0
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-05 03:21:08 -0300

    6-1: Inferência de tipos: palavra var

---
+++ 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Betotle";
+
+            int w = z; // error
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
