ef4d6cb429b1aee57db0f790c13631592ebc9cb6
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-05 03:32:00 -0300

    6-2: Sintaxe alternativa: switch-case

--- 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
+++ 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
@@ -7,15 +7,51 @@ namespace Course
         static void Main(string[] args)
         {
 
-            var x = 10;
-            var y = 20.0;
-            var z = "Betotle";
+            int x = Convert.ToInt32(Console.ReadLine());
+            string day;
 
-            int w = z; // error
+            switch (x)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
 
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            Console.WriteLine("Day: " + day);
+
+
+
+
+            //var x = 10;
+            //var y = 20.0;
+            //var z = "Betotle";
+
+            //int w = z; // error
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
 
         }
     }
