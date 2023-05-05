8cbc32130aaa6dd2a1fb157985e81b468b1e9d29
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-05 03:38:35 -0300

    6-3: Sintaxe alternativa - expressão condicional ternária

--- 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
+++ 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -7,38 +8,49 @@ namespace Course
         static void Main(string[] args)
         {
 
-            int x = Convert.ToInt32(Console.ReadLine());
-            string day;
+            double preco = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto;
 
-            switch (x)
-            {
-                case 1:
-                    day = "Sunday";
-                    break;
-                case 2:
-                    day = "Monday";
-                    break;
-                case 3:
-                    day = "Tuesday";
-                    break;
-                case 4:
-                    day = "Wednesday";
-                    break;
-                case 5:
-                    day = "Thursday";
-                    break;
-                case 6:
-                    day = "Friday";
-                    break;
-                case 7:
-                    day = "Saturday";
-                    break;
-                default:
-                    day = "Invalid value";
-                    break;
-            }
+            desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
 
-            Console.WriteLine("Day: " + day);
+            Console.WriteLine(desconto);
+
+
+
+
+
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //string day;
+
+            //switch (x)
+            //{
+            //    case 1:
+            //        day = "Sunday";
+            //        break;
+            //    case 2:
+            //        day = "Monday";
+            //        break;
+            //    case 3:
+            //        day = "Tuesday";
+            //        break;
+            //    case 4:
+            //        day = "Wednesday";
+            //        break;
+            //    case 5:
+            //        day = "Thursday";
+            //        break;
+            //    case 6:
+            //        day = "Friday";
+            //        break;
+            //    case 7:
+            //        day = "Saturday";
+            //        break;
+            //    default:
+            //        day = "Invalid value";
+            //        break;
+            //}
+
+            //Console.WriteLine("Day: " + day);
 
 
 
