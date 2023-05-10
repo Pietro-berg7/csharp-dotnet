236fb0e1394bbe0827b66fd6687c46bc1b6b2ce2
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-10 16:28:08 -0300

    7-1: Enumerações

---
+++ 07_ENUMERACOES_E_COMPOSICAO/Course/Course/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using Course.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
