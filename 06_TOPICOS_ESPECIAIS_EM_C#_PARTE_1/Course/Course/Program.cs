ce7585a78dc87f6ed8495f2e24a7b17cf150a400
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-10 09:33:03 -0300

    6-5: DateTime - representando data e hora

--- 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
+++ 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
@@ -8,35 +8,72 @@ namespace Course
         static void Main(string[] args)
         {
 
-            string original = "abcde FGHIJ ABC abc DEFG     ";
+            DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
-            string s1 = original.ToUpper();
-            string s2 = original.ToLower();
-            string s3 = original.Trim();
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
 
-            int n1 = original.IndexOf("bc");
-            int n2 = original.LastIndexOf("bc");
+            //DateTime d1 = DateTime.Parse("2000-08-15");
+            //DateTime d2 = DateTime.Parse("2000-08-15 13:05:58");
 
-            string s4 = original.Substring(3);
-            string s5 = original.Substring(3, 5);
+            //DateTime d3 = DateTime.Parse("08/15/2000");
+            //DateTime d4 = DateTime.Parse("08/15/2000 13:05:58");
 
-            string s6 = original.Replace('a', 'x');
-            string s7 = original.Replace("abc", "xy");
+            //Console.WriteLine(d1);
+            //Console.WriteLine(d2);
+            //Console.WriteLine(d3);
+            //Console.WriteLine(d4);
 
-            bool b1 = String.IsNullOrEmpty(original);
-            bool b2 = String.IsNullOrWhiteSpace(original);
+            //DateTime d1 = new DateTime(2018, 11, 25);
+            //DateTime d2 = new DateTime(2018, 11, 25, 20,45, 3);
+            //DateTime d3 = new DateTime(2018, 11, 25, 20,45, 3, 500);
 
-            Console.WriteLine("Original: -" + original + "-");
-            Console.WriteLine("ToUpper: -" + s1 + "-");
-            Console.WriteLine("ToLower: -" + s2 + "-");
-            Console.WriteLine("Trim: -" + s3 + "-");
-            Console.WriteLine("IndexOf('bc'): " + n1);
-            Console.WriteLine("LastIndexOf('bc'): " + n2);
-            Console.WriteLine("Substring(3, 5): -" + s5 + "-");
-            Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
-            Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
-            Console.WriteLine("IsNullOrEmpty: " + b1);
-            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
+            //DateTime d4 = DateTime.Now;
+            //DateTime d5 = DateTime.Today;
+            //DateTime d6 = DateTime.UtcNow;
+
+            //Console.WriteLine(d1);
+            //Console.WriteLine(d2);
+            //Console.WriteLine(d3);
+            //Console.WriteLine(d4);
+            //Console.WriteLine(d5);
+            //Console.WriteLine(d6);
+
+
+
+
+
+
+            //string original = "abcde FGHIJ ABC abc DEFG     ";
+
+            //string s1 = original.ToUpper();
+            //string s2 = original.ToLower();
+            //string s3 = original.Trim();
+
+            //int n1 = original.IndexOf("bc");
+            //int n2 = original.LastIndexOf("bc");
+
+            //string s4 = original.Substring(3);
+            //string s5 = original.Substring(3, 5);
+
+            //string s6 = original.Replace('a', 'x');
+            //string s7 = original.Replace("abc", "xy");
+
+            //bool b1 = String.IsNullOrEmpty(original);
+            //bool b2 = String.IsNullOrWhiteSpace(original);
+
+            //Console.WriteLine("Original: -" + original + "-");
+            //Console.WriteLine("ToUpper: -" + s1 + "-");
+            //Console.WriteLine("ToLower: -" + s2 + "-");
+            //Console.WriteLine("Trim: -" + s3 + "-");
+            //Console.WriteLine("IndexOf('bc'): " + n1);
+            //Console.WriteLine("LastIndexOf('bc'): " + n2);
+            //Console.WriteLine("Substring(3, 5): -" + s5 + "-");
+            //Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
+            //Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
+            //Console.WriteLine("IsNullOrEmpty: " + b1);
+            //Console.WriteLine("IsNullOrWhiteSpace: " + b2);
 
 
 
