5f0cfbbcc925c54c71cb536b80e811d993d7211f
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-10 10:47:07 -0300

    6-9: DateTimeKind e padrão ISO 8601

--- 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
+++ 06_TOPICOS_ESPECIAIS_EM_C#_PARTE_1/Course/Course/Program.cs
@@ -8,18 +8,58 @@ namespace Course
         static void Main(string[] args)
         {
 
-            TimeSpan t1 = new TimeSpan(1, 30, 10);
-            TimeSpan t2 = new TimeSpan(0, 10, 5);
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local DateTime
 
-            TimeSpan sum = t1.Add(t2);
-            TimeSpan dif = t1.Subtract(t2);
-            TimeSpan mult = t2.Multiply(2.0);
-            TimeSpan div = t2.Divide(2.0);
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine(d2.ToString("yyyy-MM-ddTHH:mm:ssZ")); // cuidado!
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
 
-            Console.WriteLine(sum);
-            Console.WriteLine(dif);
-            Console.WriteLine(mult);
-            Console.WriteLine(div);
+            //DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            //DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            //DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+            //Console.WriteLine("d1: " + d1);
+            //Console.WriteLine("d1 Kind: " + d1.Kind);
+            //Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            //Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+            //Console.WriteLine();
+            //Console.WriteLine("d2: " + d2);
+            //Console.WriteLine("d2 Kind: " + d2.Kind);
+            //Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            //Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+            //Console.WriteLine();
+            //Console.WriteLine("d3: " + d3);
+            //Console.WriteLine("d3 Kind: " + d3.Kind);
+            //Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+            //Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+
+
+
+
+
+
+            //TimeSpan t1 = new TimeSpan(1, 30, 10);
+            //TimeSpan t2 = new TimeSpan(0, 10, 5);
+
+            //TimeSpan sum = t1.Add(t2);
+            //TimeSpan dif = t1.Subtract(t2);
+            //TimeSpan mult = t2.Multiply(2.0);
+            //TimeSpan div = t2.Divide(2.0);
+
+            //Console.WriteLine(sum);
+            //Console.WriteLine(dif);
+            //Console.WriteLine(mult);
+            //Console.WriteLine(div);
 
             //TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
 
