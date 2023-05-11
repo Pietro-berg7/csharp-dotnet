6fbebd947e5bef81b6ec088b20583c6796be0907
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-11 16:52:28 -0300

    8-1: Herança

---
+++ 08_HERANCA_E_POLIMORFISMO/Course/Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using Course.Entities;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            // account.Balance = 200.0; // error
+        }
+    }
+}
