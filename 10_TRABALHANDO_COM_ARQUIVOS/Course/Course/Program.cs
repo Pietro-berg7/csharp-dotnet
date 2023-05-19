b3bcd461276a2dab947d583c0c9602659e65bc1e
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-18 22:32:34 -0300

    10-2: FileStream e StreamReader

--- 10_TRABALHANDO_COM_ARQUIVOS/Course/Course/Program.cs
+++ 10_TRABALHANDO_COM_ARQUIVOS/Course/Course/Program.cs
@@ -7,24 +7,57 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string sourcePath = @"C:\temp\file1.txt";
-            string targetPath = @"C:\temp\file2.txt";
-
+            string path = @"C:\temp\file1.txt";
+            // FileStream fs = null;
+            StreamReader sr = null;
+            
             try
             {
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
-                string[] lines = File.ReadAllLines(sourcePath);
-                foreach (string line in lines)
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
                 {
+                    string line = sr.ReadLine();
                     Console.WriteLine(line);
                 }
+                // fs = new FileStream(path, FileMode.Open);
+                // sr = new StreamReader(fs);
+                // string line = sr.ReadLine();
+                // Console.WriteLine(line);
             }
-            catch (IOException e)
+            catch(IOException e)
             {
                 Console.WriteLine("An error ocurred");
                 Console.WriteLine(e.Message);
             }
+            finally
+            {
+                // if (fs != null) fs.Close(); // fs?.Close();
+                if (sr != null) sr.Close(); // sr?.Close();
+            }
+
+
+
+
+
+
+            //string sourcePath = @"C:\temp\file1.txt";
+            //string targetPath = @"C:\temp\file2.txt";
+
+            //try
+            //{
+            //    FileInfo fileInfo = new FileInfo(sourcePath);
+            //    fileInfo.CopyTo(targetPath);
+            //    string[] lines = File.ReadAllLines(sourcePath);
+            //    foreach (string line in lines)
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error ocurred");
+            //    Console.WriteLine(e.Message);
+            //}
         }
     }
 }
