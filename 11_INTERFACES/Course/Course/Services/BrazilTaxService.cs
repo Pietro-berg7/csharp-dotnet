d40aef56ee9824048d06fff6666b019ee4ea3608
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-19 22:48:26 -0300

    11-3: Solução com interface

--- 11_INTERFACES/Course/Course/Services/BrazilTaxService.cs
+++ 11_INTERFACES/Course/Course/Services/BrazilTaxService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Course.Services
 {
-    internal class BrazilTaxService
+    internal class BrazilTaxService : ITaxService
     {
         public double Tax(double amount)
         {
---
+++ 11_INTERFACES/Course/Course/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace Course.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
