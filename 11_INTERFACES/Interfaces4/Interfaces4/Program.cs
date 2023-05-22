c51f613d1bb171743e52cbf4f273729dc484363a
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-22 18:55:12 -0300

    11-7: Interface IComparable

---
+++ 11_INTERFACES/Interfaces4/Interfaces4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Interfaces4.Entities;
+
+namespace Interfaces4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
