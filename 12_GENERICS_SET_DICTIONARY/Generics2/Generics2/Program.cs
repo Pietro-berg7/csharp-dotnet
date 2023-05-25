4d854829fef2281c13cb46f3650f6f8d9dfc1db9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-25 16:20:34 -0300

    12-3: Restrições de generics

---
+++ 12_GENERICS_SET_DICTIONARY/Generics2/Generics2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Generics2.Entities;
+using Generics2.Services;
+using System;
+using System.Globalization;
+
+namespace Generics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = Convert.ToDouble(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max:");
+            Console.WriteLine(max);
+        }
+    }
+}
