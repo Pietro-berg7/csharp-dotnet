185c4a4357c2d0b6b821e921b092f29be06bd733
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-25 18:40:27 -0300

    12-5: Conjuntos: HashSet e SortedSet PARTE 1

--- 12_GENERICS_SET_DICTIONARY/Course/Course/Program.cs
+++ 12_GENERICS_SET_DICTIONARY/Course/Course/Program.cs
@@ -7,13 +7,32 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
-            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+            HashSet<string> set = new HashSet<string>();
 
-            Console.WriteLine(a.Equals(b)); // true
-            Console.WriteLine(a == b); // false
-            Console.WriteLine(a.GetHashCode());
-            Console.WriteLine(b.GetHashCode());
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook")); // true
+            Console.WriteLine(set.Contains("Computer")); // false
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+
+
+
+
+
+            //Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            //Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            //Console.WriteLine(a.Equals(b)); // true
+            //Console.WriteLine(a == b); // false
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(b.GetHashCode());
 
 
 
