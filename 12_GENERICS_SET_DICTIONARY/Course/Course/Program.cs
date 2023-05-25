3000febec29d6889e640572b319cb7d266a56210
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-25 18:51:22 -0300

    12-5: Conjuntos: HashSet e SortedSet PARTE 2

--- 12_GENERICS_SET_DICTIONARY/Course/Course/Program.cs
+++ 12_GENERICS_SET_DICTIONARY/Course/Course/Program.cs
@@ -7,19 +7,42 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
+            // union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
 
-            Console.WriteLine(set.Contains("Notebook")); // true
-            Console.WriteLine(set.Contains("Computer")); // false
+            // intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
 
-            foreach (string p in set)
-            {
-                Console.WriteLine(p);
-            }
+            // diference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+
+
+
+
+
+            //HashSet<string> set = new HashSet<string>();
+
+            //set.Add("TV");
+            //set.Add("Notebook");
+            //set.Add("Tablet");
+
+            //Console.WriteLine(set.Contains("Notebook")); // true
+            //Console.WriteLine(set.Contains("Computer")); // false
+
+            //foreach (string p in set)
+            //{
+            //    Console.WriteLine(p);
+            //}
 
 
 
@@ -46,6 +69,15 @@ namespace Course
 
             //Console.WriteLine(a.GetHashCode());
             //Console.WriteLine(b.GetHashCode());
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.WriteLine(obj + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
