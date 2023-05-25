aba5a9cf25404dda613921da904b502857a672f0
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-25 16:20:44 -0300

    12-4: GetHashCode e Equals

---
+++ 12_GENERICS_SET_DICTIONARY/Course/Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using Course.Entities;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); // true
+            Console.WriteLine(a == b); // false
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+
+
+
+
+
+            //string a = "Maria";
+            //string b = "Alex";
+
+            //Console.WriteLine(a.Equals(b));
+
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
