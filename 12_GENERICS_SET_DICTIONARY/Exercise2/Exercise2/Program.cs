9b0530606128eadc71cb0746b1cdb973c44714dc
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-27 22:19:15 -0300

    12-10: Exercício proposto sobre Dictionary

---
+++ 12_GENERICS_SET_DICTIONARY/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string candidate = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if(dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
