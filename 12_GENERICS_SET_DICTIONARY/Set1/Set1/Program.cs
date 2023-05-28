30de6ee7cebb323d80f42dfd5e3614655776544a
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-27 21:26:27 -0300

    12-7: Exercício resolvido sobre conjuntos

---
+++ 12_GENERICS_SET_DICTIONARY/Set1/Set1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Set1.Entities;
+using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(" ");
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });                 
+                    }
+                    Console.WriteLine("Total users: " + set.Count());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
