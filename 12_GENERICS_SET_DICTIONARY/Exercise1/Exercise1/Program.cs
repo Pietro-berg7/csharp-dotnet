3944509f6ca50c163a516260ebfe9dd41002c803
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-27 21:36:17 -0300

    12-8:  Exercício proposto sobre conjuntos

---
+++ 12_GENERICS_SET_DICTIONARY/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = Convert.ToInt32(Console.ReadLine());
+                courseA.Add(code);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = Convert.ToInt32(Console.ReadLine());
+                courseB.Add(code);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = Convert.ToInt32(Console.ReadLine());
+                courseC.Add(code);
+            }
+
+            HashSet<int> all = new HashSet<int>(courseA);
+
+            all.UnionWith(courseB);
+            all.UnionWith(courseC);
+
+            Console.WriteLine("Total students: " + all.Count);
+        }
+    }
+}
