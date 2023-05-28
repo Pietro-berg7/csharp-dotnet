f1ad48d7764aa958aadc4286cd60f0c98e216347
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-27 22:41:33 -0300

    13: Extension methods

---
+++ 13_TOPICOS_ESPECIAIS_EM_C#_PARTE_2/Course/Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using Course.Extensions;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+            DateTime dt = new DateTime(2023, 05, 27, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
