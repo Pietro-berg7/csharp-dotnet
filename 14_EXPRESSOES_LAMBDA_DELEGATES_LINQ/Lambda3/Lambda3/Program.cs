e9bb145096cc1fb8afc83afd2338f4255d3a62f9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-29 15:57:57 -0300

    14-5: Action (exemplo com ForEach)

---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Lambda3/Lambda3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lambda3.Entities;
+using System;
+
+namespace Lambda3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //Action<Product> act = UpdatePrice;
+            Action<Product> act = p => { p.Price += p.Price * 0.1;  };
+
+            //list.ForEach(UpdatePrice);
+            //list.ForEach(act);
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
