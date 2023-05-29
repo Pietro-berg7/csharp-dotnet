0238522099ede946788399735b6c26bacd57b49c
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-29 19:23:00 -0300

    14-11: Exercício resolvido

---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Lambda5/Lambda5/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Lambda5.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Lambda5/Lambda5/Program.cs
@@ -0,0 +1,38 @@
+﻿using Lambda5.Entities;
+using System;
+using System.Globalization;
+
+namespace Lambda5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(",");
+                    string name = fields[0];
+                    double price = Convert.ToDouble(fields[1], CultureInfo.InvariantCulture);
+
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
