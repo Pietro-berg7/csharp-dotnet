86033e402a2fa490c834532e8bed684c847937c9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-28 23:48:08 -0300

    14-1: Uma experiência com Comparison

---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Lambda1/Lambda1/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Lambda1.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Lambda1/Lambda1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lambda1.Entities;
+using System;
+
+namespace Lambda1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            //Comparison<Product> comp = CompareProducts;
+            //Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            //list.Sort(CompareProducts);
+            //list.Sort(comp);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }
+}
