6a12f68b766c255841f304607c42859793da2834
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-29 15:43:44 -0300

    14-4: Predicate (exemplo com RemoveAll)

---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Lambda2/Lambda2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Lambda2.Entities;
+using System;
+
+namespace Lambda2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
