12818a15d48cb7880b631b6d9a54dc98f798fd2c
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-29 16:49:59 -0300

    14-7: Introdução ao LINQ

---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/LinqIntro/LinqIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinqIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
