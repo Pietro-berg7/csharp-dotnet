26c9d6ce0838c8a3a83d1ca439a24eefbb6a5564
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-29 15:26:57 -0300

    14-2: Introdução a delegates

---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Delegates1/Delegates1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Delegates1.Services;
+using System;
+
+namespace Delegates1
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            //double result = op.Invoke(a, b);
+            double result = op(a, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 14_EXPRESSOES_LAMBDA_DELEGATES_LINQ/Delegates1/Delegates1/Services/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿namespace Delegates1.Services
+{
+    internal class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
