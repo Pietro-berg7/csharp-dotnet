654dd0b17fc1675a8a18adfe89b5a6cc59de48b9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-31 04:34:25 -0300

    15-2: Encapsulamento na prática

---
+++ 15_REVISAO_POO/ExemploPOO/ExemploPOO/Models/ContaCorrente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    internal class ContaCorrente
+    {
+        public int NumeroConta { get; set; }
+        private decimal saldo;
+
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+                Console.WriteLine("Saque realixado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Valor desejado é maior que o saldo disponível!");
+            }
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("Seu saldo disponível é: " + saldo);
+        }
+    }
+}
--- 15_REVISAO_POO/ExemploPOO/ExemploPOO/Program.cs
+++ 15_REVISAO_POO/ExemploPOO/ExemploPOO/Program.cs
@@ -1,7 +1,18 @@
 ﻿using ExemploPOO.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Pietro";
-p1.Idade = 23;
+ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-p1.Apresentar();
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Pietro";
+//p1.Idade = 23;
+
+//p1.Apresentar();
