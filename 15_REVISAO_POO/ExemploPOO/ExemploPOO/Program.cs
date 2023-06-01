eb638ec77dc33a6ac6f52b80271354355cbc49c7
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-31 21:03:34 -0300

    15-5: Classe abstrata na prática

--- 15_REVISAO_POO/ExemploPOO/ExemploPOO/Program.cs
+++ 15_REVISAO_POO/ExemploPOO/ExemploPOO/Program.cs
@@ -1,17 +1,26 @@
 ﻿using ExemploPOO.Models;
 
-Aluno a1 = new Aluno();
-a1.Nome = "Pietro";
-a1.Idade = 23;
-a1.Email = "teste@teste.com";
-a1.Nota = 10;
-a1.Apresentar();
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
 
-Professor p1 = new Professor();
-p1.Nome = "Eduardo";
-p1.Idade = 30;
-p1.Salario = 1000;
-p1.Apresentar();
+
+
+
+
+
+//Aluno a1 = new Aluno();
+//a1.Nome = "Pietro";
+//a1.Idade = 23;
+//a1.Email = "teste@teste.com";
+//a1.Nota = 10;
+//a1.Apresentar();
+
+//Professor p1 = new Professor();
+//p1.Nome = "Eduardo";
+//p1.Idade = 30;
+//p1.Salario = 1000;
+//p1.Apresentar();
 
 
 
