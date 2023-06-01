ae5425e105835fd02aaeaddd57ff2bb15b5f05a0
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-31 21:16:56 -0300

    15-7: Classes e métodos selados

---
+++ 15_REVISAO_POO/ExemploPOO/ExemploPOO/Models/Diretor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    internal class Diretor : Professor  // erro se professor for sealed
+    {
+        public override void Apresentar() // erro se Apresentar for sealed
+        {
+            Console.WriteLine("Diretor");
+        }
+    }
+}
