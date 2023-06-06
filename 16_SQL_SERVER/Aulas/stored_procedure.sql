5ab3952a13fb7b8f267de7ffe11d62c4684475af
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-06 00:14:00 -0300

    16-26: Chamando Stored Procedure

--- 16_SQL_SERVER/Aulas/stored_procedure.sql
+++ 16_SQL_SERVER/Aulas/stored_procedure.sql
@@ -3,14 +3,21 @@ SELECT * FROM Produtos
 INSERT INTO Produtos (Nome, Preco, Tamanho, Genero)
 VALUES ('Produto teste 3', 10, 'G', 'M')
 
-CREATE PROCEDURE InserirNovoProduto
-@Nome varchar(255),
-@Cor varchar(50),
-@Preco decimal,
-@Tamanho varchar(5),
-@Genero char(1)
+EXEC InserirNovoProduto
+'NOVO PRODUTO PROCEDURE',
+'COLORIDO',
+50,
+'G',
+'U'
 
-AS
+--CREATE PROCEDURE InserirNovoProduto
+--@Nome varchar(255),
+--@Cor varchar(50),
+--@Preco decimal,
+--@Tamanho varchar(5),
+--@Genero char(1)
 
-INSERT INTO Produtos (Nome, Cor, Preco, Tamanho, Genero)
-VALUES (@Nome, @Cor, @Preco, @Tamanho, @Genero)
+--AS
+
+--INSERT INTO Produtos (Nome, Cor, Preco, Tamanho, Genero)
+--VALUES (@Nome, @Cor, @Preco, @Tamanho, @Genero)
