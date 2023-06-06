1bf519847701b5062bc127063334e83d89413c67
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-05 23:49:41 -0300

    16-19: Constraint NOT NULL

---
+++ 16_SQL_SERVER/Aulas/not_null.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Produtos
+
+--INSERT INTO Produtos (Nome, Cor, Tamanho, Genero, DataCadastro)
+--VALUES ('Nome', 'Cor', 'G', 'M', GETDATE())
+
+INSERT INTO Produtos (Nome, Preco, Tamanho, Genero, DataCadastro)
+VALUES ('Nome', 10, 'G', 'M', GETDATE())
