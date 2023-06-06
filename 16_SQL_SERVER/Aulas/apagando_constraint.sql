7a783bad693347ce26ac53e797c5b2897674663a
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-06 00:02:54 -0300

    16-24: Apagando uma Constraint

---
+++ 16_SQL_SERVER/Aulas/apagando_constraint.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Produtos
+
+INSERT INTO Produtos (Nome, Preco, Tamanho, Genero)
+VALUES ('Produto teste 3', 10, 'G', 'M')
+
+ALTER TABLE Produtos
+DROP CONSTRAINT UQ__Produtos__7D8FE3B2FF993D9E
