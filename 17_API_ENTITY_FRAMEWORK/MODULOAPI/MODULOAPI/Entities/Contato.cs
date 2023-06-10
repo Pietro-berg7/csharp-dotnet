e54c2f7fbff1a38b6b49ef87fced741859b76b43
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-09 23:46:01 -0300

    17-5: Criando a classe entidade

---
+++ 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Entities/Contato.cs
@@ -0,0 +1,12 @@
+﻿namespace MODULOAPI.Entities
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+
+
+    }
+}
