9aa0efe130ffea8e8351963c85bf6e4ddd825f9c
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-11 07:00:50 -0300

    17-7: Configurando a conexão

--- 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Context/AgendaContext.cs
+++ 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Context/AgendaContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MODULOAPI.Entities;
 
 namespace MODULOAPI.Context
 {
-    public class AgendaContext : DbContext
+    public class AgendaContext: DbContext
     {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Contato> Contatos { get; set; }
     }
 }
