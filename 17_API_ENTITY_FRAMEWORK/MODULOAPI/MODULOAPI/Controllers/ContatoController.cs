1a7e4da2ef9de6adf24973c6127e0ee4f4686255
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-11 20:31:34 -0300

    17-11: Criando o endpoint de update

--- 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Controllers/ContatoController.cs
+++ 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Controllers/ContatoController.cs
@@ -33,5 +33,23 @@ namespace MODULOAPI.Controllers
 
             return Ok(contato);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
     }
 }
