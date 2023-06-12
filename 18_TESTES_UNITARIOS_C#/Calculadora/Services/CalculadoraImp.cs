196baef7f9da30ad979954e38002404c1a46ae74
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-12 18:47:15 -0300

    18-1: Implementando a classe calculadora

---
+++ 18_TESTES_UNITARIOS_C#/Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    internal class CalculadoraImp
+    {
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
