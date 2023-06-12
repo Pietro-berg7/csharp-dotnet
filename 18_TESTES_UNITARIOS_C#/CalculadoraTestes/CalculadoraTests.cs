dd96effbde11261487ab0bfc740c2ba4722b5cae
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-12 19:02:51 -0300

    18-3: Implementando o teste unitário

--- 18_TESTES_UNITARIOS_C#/CalculadoraTestes/CalculadoraTests.cs
+++ 18_TESTES_UNITARIOS_C#/CalculadoraTestes/CalculadoraTests.cs
@@ -6,7 +6,7 @@ public class CalculadoraTests
 {
     private CalculadoraImp _calc;
 
-    private CalculadoraTests()
+    public CalculadoraTests()
     {
         _calc = new CalculadoraImp();
     }
@@ -15,7 +15,13 @@ public class CalculadoraTests
     public void DeveSomar5Com10ERetornar15()
     {
         // Arrange
+        int num1 = 5;
+        int num2 = 10;
+
         // Act
+        int resultado = _calc.Somar(num1, num2);
+
         // Assert
+        Assert.Equal(15, resultado);
     }
 }
