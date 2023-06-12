b4dc2504809536165e4c84c0ab3681097f3a8dec
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-11 21:45:46 -0300

    17-13: Criando o endpoint de obter por nome

--- 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Controllers/ContatoController.cs
+++ 17_API_ENTITY_FRAMEWORK/MODULOAPI/MODULOAPI/Controllers/ContatoController.cs
@@ -34,6 +34,13 @@ namespace MODULOAPI.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            return Ok(contatos);
+        }
+
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Contato contato)
         {
